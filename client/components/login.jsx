fcb89c15039f462d28d02efb77487efbb085504e
Grant Kang <3s.grantkang@gmail.com>
2020-05-04 15:29:26 -0700

    changed GET request to backend to POST

--- client/components/login.jsx
+++ client/components/login.jsx
@@ -1,7 +1,10 @@
 import React, { Fragment } from 'react';
+import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
 import Input from '@material-ui/core/Input';
 import Button from '@material-ui/core/Button';
+
+import AppContext from '../lib/context';
 
 export default class Login extends React.Component {
   constructor(props) {
@@ -11,12 +14,34 @@ export default class Login extends React.Component {
       password: ''
     };
     this.onChange = this.onChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   onChange(event) {
     const newState = {};
     newState[event.target.name] = event.target.value;
     this.setState(newState);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const loginInput = {};
+    loginInput.email = this.state.email;
+    loginInput.password = this.state.password;
+
+    const req = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(loginInput)
+    };
+    fetch('/api/login', req)
+      .then(response => response.json())
+      .then(data => {
+        this.context.onLogin(data);
+
+      });
   }
 
   render() {
@@ -27,24 +52,27 @@ export default class Login extends React.Component {
             <Grid item xs={0} md={4} />
             <Grid item container xs={12} md={4} direction="column">
               <form onSubmit={this.handleSubmit}>
-                <Input
-                  placeholder="email"
-                  type="email"
-                  name="email"
-                  required
-                  inputProps={{ 'aria-label': 'description' }}
-                  onChange={this.onChange} />
-                <Input
-                  placeholder="password"
-                  type="password"
-                  name="password"
-                  required
-                  inputProps={{ 'aria-label': 'description' }}
-                  onChange={this.onChange} />
-                <Button
-                  color="primary"
-                  variant="contained"
-                  type="submit">Log In</Button>
+                <Box display="flex" flexDirection="column" justifyContent="center">
+                  <Input
+                    placeholder="email"
+                    type="email"
+                    name="email"
+                    required
+                    inputProps={{ 'aria-label': 'description' }}
+                    onChange={this.onChange} />
+                  <Input
+                    placeholder="password"
+                    type="password"
+                    name="password"
+                    required
+                    inputProps={{ 'aria-label': 'description' }}
+                    onChange={this.onChange} />
+                  <Button
+                    color="primary"
+                    variant="contained"
+                    type="submit">Log In</Button>
+                </Box>
+
               </form>
             </Grid>
             <Grid item xs={0} md={4} />
@@ -54,3 +82,5 @@ export default class Login extends React.Component {
     );
   }
 }
+
+Login.contextType = AppContext;
