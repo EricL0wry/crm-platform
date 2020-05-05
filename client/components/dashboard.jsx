5172250c16ab80d0154a242cf0060b0fc9cca22e
Yizhou Liu <yizhou.liu.mail@gmail.com>
2020-05-05 15:50:51 -0700

    added fetch call func in dashboard component (w/o test)

--- client/components/dashboard.jsx
+++ client/components/dashboard.jsx
@@ -1,12 +1,29 @@
-import React, { Fragment } from 'react';
-import MenuAppBar from './menu_app_bar';
-export default class Dashboard extends React.Component {
-  render() {
-    return (
-      <Fragment>
-        <MenuAppBar />
-        <h1 align="center">Dashboard</h1>
-      </Fragment>
-    );
-  }
+import React, { Fragment, useContext, useEffect } from 'react';
+import AppContext from '../lib/context';
+import MenuAppBar from './menu-app-bar';
+import TicketList from './ticket-list';
+import Weather from './weather';
+export default function Dashboard() {
+
+  const context = useContext(AppContext);
+  const userId = context.getUser();
+  let myData = '';
+  useEffect(() => {
+    fetch('/api/dashboard/' + userId)
+      .then(response => response.json())
+      .then(data => {
+        myData = data;
+        console.log(myData);
+      })
+      .catch(error => console.log('Fetch failed!', error));
+  });
+
+  return (
+    <Fragment>
+      <MenuAppBar />
+      <Weather />
+      <TicketList />
+    </Fragment>
+  );
+
 }
