131bc79d2bc29a105e9d94db81716761f895133e
Yizhou Liu <yizhou.liu.mail@gmail.com>
2020-05-06 10:51:26 -0700

    moved header navbar outside of dashboard component

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -3,6 +3,7 @@ import Login from './login';
 import DashBoard from './dashboard';
 import AppContext from '../lib/context';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import MenuAppBar from './menu-app-bar';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -55,6 +56,7 @@ export default class App extends React.Component {
       return (
         <AppContext.Provider value={this.contextValue}>
           <BrowserRouter>
+            <MenuAppBar />
             <Switch>
               <Route exact path="/" component={DashBoard} />
               {/* <Route path="/" component={Login} /> */}
