f06ec6989f36a9710819fe223622e3617716b170
Eric Lowry <ericmichaellowry@gmail.com>
2020-05-06 15:34:53 -0700

    Imported material ui components needed and rendered a card element with member first name as test

--- client/components/organization-list-item.jsx
+++ client/components/organization-list-item.jsx
@@ -1,8 +1,36 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import Icon from '@material-ui/core/Icon';
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: '100%'
+  },
+  text: {
+    fontSize: 14
+  },
+  card: {
+    width: '100%',
+    padding: '8px',
+    '&:last-child': {
+      padding: '8px'
+    }
+  }
+});
 
 export default function OrganizationListItem(props) {
+  const classes = useStyles();
+  const { member } = props;
 
   return (
-    <h1>org list item</h1>
+    <Card className={classes.root}>
+      <Typography>
+        {member.firstName}
+      </Typography>
+    </Card>
   );
 }
