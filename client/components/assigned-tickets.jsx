4280c840389f5b838804418f285cc55acb1a94e3
Kevin Kim <kevinnhkim@live.com>
2020-05-07 00:35:07 -0700

    Impleneted useEffect to make fetch call for data needed for list of tickets

--- client/components/assigned-tickets.jsx
+++ client/components/assigned-tickets.jsx
@@ -3,5 +3,16 @@ import AppContext from '../lib/context';
 import TicketList from './ticket-list';
 
 export default function AssignedTickets() {
+  const context = useContext(AppContext);
+  const [tickets, setTickets] = useState(null);
+  const id = context.getUser().userId;
+
+  useEffect(() => {
+    fetch('/api/assignedtickets/' + { id })
+      .then(res => res.json())
+      .then(data => {
+        setTickets(data);
+      }).catch(error => console.log(error));
+  }, []);
 
 }
