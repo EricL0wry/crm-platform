2a7a4c462136a283b70695e302c20ca4bd59b948
Kevin Kim <kevinnhkim@live.com>
2020-05-06 18:04:09 -0700

    Imported useContext, useEffect, useState, AppContext, TicketList and created function component

---
+++ client/components/assigned-tickets.jsx
@@ -0,0 +1,7 @@
+import React, { useContext, useEffect, useState } from 'react';
+import AppContext from '../lib/context';
+import TicketList from './ticket-list';
+
+export default function AssignedTickets() {
+
+}
