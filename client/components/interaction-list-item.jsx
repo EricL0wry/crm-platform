5c258dc73d13512176a5d9a113408c9ae746f659
Yizhou Liu <yizhou.liu.mail@gmail.com>
2020-05-07 12:01:55 -0700

    Fixed typo

--- client/components/interaction-list-item.jsx
+++ client/components/interaction-list-item.jsx
@@ -1,7 +1,7 @@
 import React, { Fragment } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
-import Grid from '@material-ui/core/Grid';\
+import Grid from '@material-ui/core/Grid';
 
 const useStyles = makeStyles({
   root: {
