fd7fdde0e0da5d86f848146e3c7a70fa12e76dfa
Kevin Kim <kevinnhkim@live.com>
2020-05-07 12:23:15 -0700

    Wrapped list item card with link component to be able view ticket details

--- client/components/ticket-list-item.jsx
+++ client/components/ticket-list-item.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
@@ -25,33 +26,38 @@ export default function TicketListItem(props) {
   const priorities = ['', 'Hign', 'Medium', 'Low'];
   const ticket = props.ticket;
   return (
-    <Card className={classes.root} onClick={null}>
-      <CardContent className= {classes.card}>
-        <Grid container spacing={3}>
-          <Grid item xs={6}>
-            <Typography className={classes.text} color="textSecondary" >
-              TKT: {ticket.ticketId}
-            </Typography>
-            <Typography className={classes.text} color="textSecondary" >
-              Name: {ticket.firstName + ' ' + ticket.lastName}
-            </Typography>
-            <Typography className={classes.text} color="textSecondary" >
-              Task: {ticket.description}
-            </Typography>
+    <Link
+      to={`/tickets/${ticket.ticketId}`}
+      style={{ textDecoration: 'none', minWidth: '100%' }}
+    >
+      <Card className={classes.root} onClick={null}>
+        <CardContent className={classes.card}>
+          <Grid container spacing={3}>
+            <Grid item xs={6}>
+              <Typography className={classes.text} color="textSecondary">
+                TKT: {ticket.ticketId}
+              </Typography>
+              <Typography className={classes.text} color="textSecondary">
+                Name: {ticket.firstName + ' ' + ticket.lastName}
+              </Typography>
+              <Typography className={classes.text} color="textSecondary">
+                Task: {ticket.description}
+              </Typography>
+            </Grid>
+            <Grid item xs={6}>
+              <Typography className={classes.text} color="textSecondary">
+                Priority: {priorities[ticket.priority]}
+              </Typography>
+              <Typography className={classes.text} color="textSecondary">
+                Due: {ticket.dueDate}
+              </Typography>
+              <Typography className={classes.text} color="textSecondary">
+                Created by: {ticket.ownerFirstName + ' ' + ticket.ownerLastName}
+              </Typography>
+            </Grid>
           </Grid>
-          <Grid item xs={6}>
-            <Typography className={classes.text} color="textSecondary" >
-              Priority: {priorities[ticket.priority]}
-            </Typography>
-            <Typography className={classes.text} color="textSecondary" >
-              Due: {ticket.dueDate}
-            </Typography>
-            <Typography className={classes.text} color="textSecondary" >
-              Created by: {ticket.ownerFirstName + ' ' + ticket.ownerLastName}
-            </Typography>
-          </Grid>
-        </Grid>
-      </CardContent>
-    </Card>
+        </CardContent>
+      </Card>
+    </Link>
   );
 }
