a724cad473822ea8bdcefd965bea187be17a5f75
Eric Lowry <ericmichaellowry@gmail.com>
2020-05-09 10:46:52 -0700

    Initial component setup

---
+++ client/components/map-dialog.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import Dialog from '@material-ui/core/Dialog';
+
+export default function MapDialog() {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <Dialog>
+
+    <Dialog />
+  );
+}
