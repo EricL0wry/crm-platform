7e58a0e880e7a1aab83b8b06b82c805483269478
Eric Lowry <ericmichaellowry@gmail.com>
2020-05-09 12:02:50 -0700

    Updated url for image sizing and added classes to control image size in dialog

--- client/components/map-dialog.jsx
+++ client/components/map-dialog.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import Dialog from '@material-ui/core/Dialog';
 import DialogTitle from '@material-ui/core/DialogTitle';
-import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import IconButton from '@material-ui/core/Icon';
 import { Typography } from '@material-ui/core';
@@ -9,8 +9,18 @@ import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
 import List from '@material-ui/core/List';
 
+const useStyles = makeStyles(theme => {
+  return {
+    mapImg: {
+      width: '100%',
+      objectFit: 'contain'
+    }
+  };
+});
+
 export default function MapDialog(props) {
   const [open, setOpen] = useState(false);
+  const classes = useStyles();
 
   const handleClickOpen = () => setOpen(true);
 
@@ -24,6 +34,7 @@ export default function MapDialog(props) {
         onClose={handleClose}
         aria-labelledby="location-title"
         aria-describedby="location-dialog-description"
+        fullWidth
       >
         <DialogTitle id="location-title">
           <Box>
@@ -48,7 +59,7 @@ export default function MapDialog(props) {
               </Grid>
             </Grid>
           </List>
-          <img src="https://maps.googleapis.com/maps/api/staticmap?zoom=13&size=300x450&markers=size:mid%7Ccolor:red%7Clabel:W%7C%229200%20Irvine%20Center%20Dr.,%20Irvine,%20CA,%2092618%22&center=%229200%20Irvine%20Center%20Dr.,%20Irvine,%20CA,%2092618%22&key=AIzaSyDrfcCNcukCbWUX1qpoa6g42UOG7f0i550" alt="Customer Location Map"/>
+          <img className={classes.mapImg} src="https://maps.googleapis.com/maps/api/staticmap?zoom=13&size=290x450&markers=size:mid%7Ccolor:red%7Clabel:W%7C%229200%20Irvine%20Center%20Dr.,%20Irvine,%20CA,%2092618%22&center=%229200%20Irvine%20Center%20Dr.,%20Irvine,%20CA,%2092618%22&key=AIzaSyDrfcCNcukCbWUX1qpoa6g42UOG7f0i550" alt="Customer Location Map"/>
 
         </DialogContent>
       </Dialog>
