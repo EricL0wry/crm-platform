93edfce25c5b821e499dc2cec1a0961a4b3e048a
Eric Lowry <ericmichaellowry@gmail.com>
2020-05-09 13:23:39 -0700

    Added remianing address fields and dialog styling. Added dialog action buttons

--- client/components/map-dialog.jsx
+++ client/components/map-dialog.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import DialogContent from '@material-ui/core/DialogContent';
+import DialogActions from '@material-ui/core/DialogActions';
 import IconButton from '@material-ui/core/Icon';
 import { Typography } from '@material-ui/core';
 import Box from '@material-ui/core/Box';
@@ -11,9 +13,15 @@ import List from '@material-ui/core/List';
 
 const useStyles = makeStyles(theme => {
   return {
+    dialog: {
+      minHeight: '663px'
+    },
     mapImg: {
       width: '100%',
       objectFit: 'contain'
+    },
+    grey: {
+      backgroundColor: theme.background.darkgrey
     }
   };
 });
@@ -35,8 +43,9 @@ export default function MapDialog(props) {
         aria-labelledby="location-title"
         aria-describedby="location-dialog-description"
         fullWidth
+        className={classes.dialog}
       >
-        <DialogTitle id="location-title">
+        <DialogTitle id="location-title" >
           <Box>
             <Typography variant="h4">
               Location
@@ -47,21 +56,55 @@ export default function MapDialog(props) {
 
           <List>
             <Grid container spacing={2}>
-              <Grid item xs={4}>
-                <Typography>
-                  Name
-                </Typography>
+              <Grid className={classes.grey} item xs={4}>
+                <Box display='flex' alignItems='center' height='100%'>
+                  <Typography>
+                    Name
+                  </Typography>
+                </Box>
               </Grid>
               <Grid item xs={8}>
                 <Typography>
                     Will Billiamson
                 </Typography>
               </Grid>
+              <Grid className={classes.grey} item xs={4}>
+                <Box display='flex' alignItems='center' height='100%'>
+                  <Typography>
+                    Street
+                  </Typography>
+                </Box>
+              </Grid>
+              <Grid item xs={8}>
+                <Typography>
+                  12345 Bill Avenue
+                </Typography>
+              </Grid>
+              <Grid className={classes.grey} item xs={4}>
+                <Box display='flex' alignItems='center' height='100%'>
+                  <Typography>
+                    City/State
+                  </Typography>
+                </Box>
+              </Grid>
+              <Grid item xs={8}>
+                <Typography>
+                  New York, NY, 12345
+                </Typography>
+              </Grid>
             </Grid>
           </List>
-          <img className={classes.mapImg} src="https://maps.googleapis.com/maps/api/staticmap?zoom=13&size=290x450&markers=size:mid%7Ccolor:red%7Clabel:W%7C%229200%20Irvine%20Center%20Dr.,%20Irvine,%20CA,%2092618%22&center=%229200%20Irvine%20Center%20Dr.,%20Irvine,%20CA,%2092618%22&key=AIzaSyDrfcCNcukCbWUX1qpoa6g42UOG7f0i550" alt="Customer Location Map"/>
-
         </DialogContent>
+        <img className={classes.mapImg} src="https://maps.googleapis.com/maps/api/staticmap?zoom=13&size=290x375&markers=size:mid%7Ccolor:red%7Clabel:W%7C%229200%20Irvine%20Center%20Dr.,%20Irvine,%20CA,%2092618%22&center=%229200%20Irvine%20Center%20Dr.,%20Irvine,%20CA,%2092618%22&key=AIzaSyDrfcCNcukCbWUX1qpoa6g42UOG7f0i550" alt="Customer Location Map" />
+
+        <DialogActions>
+          <Button color="primary" autoFocus>
+            Google
+          </Button>
+          <Button onClick={handleClose} color="primary">
+            Close
+          </Button>
+        </DialogActions>
       </Dialog>
     </div>
   );
