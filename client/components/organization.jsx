0f35cae574ab1008b824609d8eedda313bd23e48
Eric Lowry <ericmichaellowry@gmail.com>
2020-05-12 14:51:54 -0700

    Added styling to organization add icon

--- client/components/organization.jsx
+++ client/components/organization.jsx
@@ -5,11 +5,21 @@ import OrganizationList from './organization-list';
 import Box from '@material-ui/core/Box';
 import { Link } from 'react-router-dom';
 import IconButton from '@material-ui/core/Icon';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => {
+  return {
+    icon: {
+      color: theme.palette.primary.light
+    }
+  };
+});
 
 export default function Organization() {
   const context = useContext(AppContext);
   const userId = context.getUser().userId;
   const [orgData, setOrgData] = useState([]);
+  const classes = useStyles();
 
   useEffect(() => {
     fetch(`/api/org/${userId}`)
@@ -30,7 +40,7 @@ export default function Organization() {
         </Box>
         <Box mr={1}>
           <Link to="/organization/newuser">
-            <IconButton>add_circle</IconButton>
+            <IconButton className={classes.icon}>add_circle</IconButton>
           </Link>
         </Box>
       </Box>
