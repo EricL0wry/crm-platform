291201ef1964b7465201bb8687fe21e09b2c4d40
Eric Lowry <ericmichaellowry@gmail.com>
2020-05-12 13:01:02 -0700

    Updated theme colors to specify light and medium variants

--- client/theme.js
+++ client/theme.js
@@ -1,11 +1,13 @@
 import { createMuiTheme } from '@material-ui/core/styles';
-import indigo from '@material-ui/core/colors/indigo';
 import purple from '@material-ui/core/colors/purple';
 import grey from '@material-ui/core/colors/grey';
 
 const theme = createMuiTheme({
   palette: {
-    primary: indigo,
+    primary: {
+      main: '#3F51B5',
+      light: '#5EA7DB'
+    },
     secondary: purple
   },
   status: {
@@ -15,7 +17,6 @@ const theme = createMuiTheme({
     darkgrey: grey[300],
     lightgrey: grey[100]
   }
-
 });
 
 export default theme;
