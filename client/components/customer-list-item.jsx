e7ee40f0c2bffd00cadf487cbf0569071096d827
Grant Kang <3s.grantkang@gmail.com>
2020-05-13 15:04:02 -0700

    fixed some layout issues

--- client/components/customer-list-item.jsx
+++ client/components/customer-list-item.jsx
@@ -9,7 +9,7 @@ import Icon from '@material-ui/core/Icon';
 import { phoneNumberFormater } from '../lib/helper-functions';
 import Avatar from '@material-ui/core/Avatar';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
   root: {
     minWidth: '100%',
     backgroundColor: '#fefefa'
@@ -18,7 +18,8 @@ const useStyles = makeStyles({
     fontSize: 14
   },
   icon: {
-    fontSize: 20
+    fontSize: 20,
+    marginRight: '4px'
   },
   card: {
     width: '100%',
@@ -26,8 +27,12 @@ const useStyles = makeStyles({
     '&:last-child': {
       padding: '8px'
     }
+  },
+  avatar: {
+    width: '90%',
+    height: '100%'
   }
-});
+}));
 
 export default function CustomerListItem(props) {
   const classes = useStyles();
@@ -36,29 +41,34 @@ export default function CustomerListItem(props) {
     <Link to={`/customers/${customer.customerId}`} style={{ textDecoration: 'none', minWidth: '100%' }}>
       <Card className={classes.root} onClick={null} elevation={4}>
         <CardContent className={classes.card}>
-          <Grid container alignItems="center">
-            <Grid item xs={1}>
+          <Grid container alignItems="center" spacing={3} justify="space-between">
+            <Grid container item xs={3} sm={1} justify="center">
               <Avatar
+                className={classes.avatar}
                 alt="customer"
                 src={customer.imagePath || '/images/users/placeholder.png'}
               />
             </Grid>
-            <Grid item xs={3}>
-              <Typography className={classes.text} color="textSecondary" >
-                {customer.firstName + ' ' + customer.lastName}
-              </Typography>
-            </Grid>
-            <Grid item xs={1}>
-              <Icon className={classes.icon}>phone</Icon>
-              <Icon className={classes.icon}>email</Icon>
-            </Grid>
-            <Grid item xs={7}>
-              <Typography className={classes.text} color="textSecondary" >
-                {phoneNumberFormater(customer.phoneNumber)}
-              </Typography>
-              <Typography className={classes.text} color="textSecondary" >
-                {customer.email}
-              </Typography>
+            <Grid container item xs={9} sm={2} direction="column" spacing={1}>
+              <Grid container item>
+                <Icon className={classes.icon}>person</Icon>
+                <Typography className={classes.text} color="textSecondary" >
+                  {customer.firstName + ' ' + customer.lastName}
+                </Typography>
+              </Grid>
+
+              <Grid container item>
+                <Icon className={classes.icon}>phone</Icon>
+                <Typography className={classes.text} color="textSecondary" >
+                  {phoneNumberFormater(customer.phoneNumber)}
+                </Typography>
+              </Grid>
+              <Grid container item>
+                <Icon className={classes.icon}>email</Icon>
+                <Typography className={classes.text} color="textSecondary" >
+                  {customer.email}
+                </Typography>
+              </Grid>
             </Grid>
           </Grid>
         </CardContent>
